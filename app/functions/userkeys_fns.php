0d5002c136faf3b48de6c13120ed8c91ed6b42fa
student <student@itstep1.dp.ua>
2018-12-17 20:25:37 +0200

    Add handler for getter and setter categories

--- app/functions/userkeys_fns.php
+++ app/functions/userkeys_fns.php
@@ -1,1 +1,11 @@
 <?php
+
+function getCategoriesByUserId($user_id){
+    $getAllCategories = fs_getAll(fs_filename("user_categories"));
+    $userCategories = [];
+    return $userCategories;
+}
+
+function addCategoryByUserId($cat, $user_id){
+
+}
